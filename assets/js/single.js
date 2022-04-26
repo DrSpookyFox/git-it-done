5da38ec423e7e466f89e49cf8d75a13cf8ae4144
Alexander Singer <96636056+DrSpookyFox@users.noreply.github.com>
2022-04-26 17:31:28 -0400

    added redirect and git issues are linked

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
+var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning")
 var issueContainerEl = document.querySelector("#issues-container");
+var queryString = document.location.search;
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -19,10 +21,30 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
       });
 };
+
+var getRepoName = function(){
+     // grab repo name from url query string
+     var queryString = document.location.search;
+     var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+   
+
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -78,4 +100,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
